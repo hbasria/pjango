7e3bf081d8f9ab3ab8d7e908233cea95846c2ae2
hbasria <hbasria@67.215.77.132>
2013-01-24 14:20:29 +0200

    HiddenField kaldırıldı

--- Pjango/Core/Forms/PjangoMediaForm.php
+++ Pjango/Core/Forms/PjangoMediaForm.php
@@ -10,15 +10,14 @@ use Pjango\Phorm,
 
 class PjangoMediaForm extends Phorm {
     protected function define_fields(){
-        $this->id = new HiddenField();
         $this->content_type_id = new HiddenField();
         $this->object_id = new HiddenField();
 
-        $this->file_path = new TextField(pjango_gettext("File Path"), 35, 255);
+        $this->file_path = new TextField(pjango_gettext("File Path"), 35, 255, array(), array('class'=>'pjangoMedia'));
         $this->file_name = new TextField(pjango_gettext("File Name"), 35, 255);        
         $this->description = new LargeTextField(pjango_gettext("Description or Embed Code"), 3, 35);
         $this->is_default = new BooleanField(pjango_gettext("Is Default"));
-        $this->is_active = new BooleanField(pjango_gettext("Is Active"));
+        //$this->is_active = new BooleanField(pjango_gettext("Is Active"));
         
         $this->meta_link_title = new TextField(pjango_gettext('Link Title'), 25, 200);
         $this->meta_link_alt = new TextField(pjango_gettext('Link Alt'), 25, 200);        

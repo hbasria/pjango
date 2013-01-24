250ac656c4c1a43356205cf34500d0e5af432abe
hbasria <hbasria@67.215.77.132>
2013-01-24 14:19:57 +0200

    site için site_type ve status eklendi

--- Pjango/Core/Forms/SiteForm.php
+++ Pjango/Core/Forms/SiteForm.php
@@ -7,9 +7,10 @@ use Pjango\Phorm,
 
 class SiteForm extends Phorm {
     protected function define_fields(){
-    	$this->parent_id = new DropDownField(pjango_gettext("Parent Site"), \Site::findAllAsChoice());
-    	
+    	$this->parent_id = new DropDownField(pjango_gettext("Parent Site"), \Site::findAllAsChoice());    	
+    	$this->site_type = new DropDownField(__("Site Type"), \Site::getTypeChoices());
         $this->domain = new TextField(__("Site Domain"), 35, 255);
         $this->name = new TextField(__("Site Name"), 35, 255);        
+        $this->status = new DropDownField(__("Site Status"), \Site::getStatusChoices());
     }
 }
